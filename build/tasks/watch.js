1d01dd7a03f9f3a0145373091dabae28a4febf49
Tristan Smith <darcnite3000@gmail.com>
2015-04-08 10:01:27 +1000

    add asset watch

--- build/tasks/watch.js
+++ build/tasks/watch.js
@@ -6,6 +6,7 @@ gulp.task('watch',['serve'], function() {
   gulp.watch(paths.scripts.source, ['scripts']);
   gulp.watch(paths.html.source, ['html']);
   gulp.watch(paths.styles.globs, ['styles']);
+  gulp.watch(paths.assets.globs, ['assets']);
 
   gulp.watch(paths.output+"**/*.*").on('change', reload);
   gulp.watch(paths.serverRoot+"index.html").on('change', reload);
